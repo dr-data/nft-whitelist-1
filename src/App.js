a660e168e34c6827b79bb07d02cf802e6a47f7c2
BuildTools <gab.mercier51@gmail.com>
2022-03-29 14:36:09 +0200

    Made the connection to metamask

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
 function App() {
+
+  const [loader, setLoader] = useState(true);
+  const [accounts, setAccounts] = useState([]);
+  const [balance, setBalance] = useState();
+  const [succes, setSucces] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getAccounts();
+    setLoader(false);
+  }, [])
+
+  async function getAccounts() {
+    if(typeof window.ethereum !== 'undefined') {
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+      setAccounts(accounts);
+      console.log(accounts);
+    }
+  }
+
+
+
   return (
     <div className="App">
+      {!loader &&
+        accounts.length > 0 ?
+        <p>You are connected with {accounts[0]}</p>
+        :
+        <p>You are not connected</p>
+      }
     </div>
   );
 }
